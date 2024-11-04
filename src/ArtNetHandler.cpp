92382da8f2f81504631da904d31b8fbd94d67267
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2024-11-04 19:09:51 +0100

    Fix brightness channel again

--- src/ArtNetHandler.cpp
+++ src/ArtNetHandler.cpp
@@ -12,7 +12,7 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *d
 {
   if (universe == ::universe)
   {
-    uint8_t brightness = data[startAddress];
+    uint8_t brightness = data[startAddress-1];
     CRGB color         = CRGB::Black;
 
     if (startAddress + 2 < length)
