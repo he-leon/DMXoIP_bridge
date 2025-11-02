05349c91bd229674a51e24cc20853215470b55ce
Lenni <leonhard.heizinger@heleon.de>
2025-11-02 19:02:11 +0100

    Added setting color per pixel

--- src/ArtNetHandler.cpp
+++ src/ArtNetHandler.cpp
@@ -7,6 +7,7 @@ ArtnetWifi artnet;
 RgbColor currentColor(0);
 uint8_t currentBrightness = DEFAULT_BRIGHTNESS;
 
+#ifdef SINGLE_COLOR
 void onDmxFrame(uint16_t universeIn, uint16_t length, uint8_t sequence, uint8_t* data)
 {
   if (universeIn != ::universe)
@@ -50,6 +51,53 @@ void onDmxFrame(uint16_t universeIn, uint16_t length, uint8_t sequence, uint8_t*
     }
   }
 }
+#else
+
+void onDmxFrame(uint16_t universeIn, uint16_t length, uint8_t sequence, uint8_t* data)
+{
+  if (universeIn != ::universe)
+    return;
+
+  if (startAddress - 1 < 0 || (startAddress - 1 + 2) >= length)
+    return;
+
+
+  for (int i = 0; i < numLeds; i++) {
+      uint8_t brightness = data[i + startAddress - 1];
+      RgbColor color(
+        data[i + startAddress],
+        data[i + startAddress + 1],
+        data[i + startAddress + 2]);
+
+      bool needsUpdate = false;
+      
+      if (brightness != currentBrightness) {
+        currentBrightness = brightness;
+        needsUpdate = true;
+      }
+
+      if (color != currentColor) {
+        currentColor = color;
+        needsUpdate = true;
+      }
+
+      if (needsUpdate) {
+        RgbColor scaledColor = currentColor;
+        scaledColor.Dim(currentBrightness);
+        strip->SetPixelColor(i, scaledColor);
+      }
+    }
+
+    strip->Show();
+    
+    // Only calculate power occasionally, not on every frame
+    static unsigned long lastPowerCalc = 0;
+    if (millis() - lastPowerCalc > 1000) { // Every second
+      calculatePowerUsage();
+      lastPowerCalc = millis();
+    }
+  }
+#endif
 
 void setupArtNet()
 {
